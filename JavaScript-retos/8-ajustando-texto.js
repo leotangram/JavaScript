5f4cb0ce8bec6053d71d602ef1a33519b31480ff
leotangram <leotangram@gmail.com>
2017-11-21 10:51:27 -0500

    Actualización

--- JavaScript-retos/8-ajustando-texto.js
+++ JavaScript-retos/8-ajustando-texto.js
@@ -1,3 +1,25 @@
-function adjustText(string, num) {
-   return string.slice(num)
+function adjustText(str, length) {
+  if (str.length > length) {
+     str = str.substring(0, length);
+  } else {
+    while (str.length < length) {
+      str += " ";
+    }
+  }
+
+  return str;
+}
+
+
+// Otra solución
+function adjustText(str, length) {
+  while (str.length < length) {
+    str += " "; 
+  }
+
+  if (str.length > length) {
+     str = str.substring(0, length);
+  }
+
+  return str;
 }
